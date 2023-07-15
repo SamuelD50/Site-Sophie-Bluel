99be941285eec798311f218890515b1b6a78a5dc
Samuel Duflos <duflos.samuel@gmail.com>
2023-07-15 17:10:10 +0200

    Etape 1.2 : Réalisation du filtre des travaux

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,22 +1,84 @@
+// Etape 1 : Créez la page de présentation des travaux à partir du HTML existant
+
 // Etape 1.1 : Récupération des travaux depuis le back-end
+
+let works = [];
 
 fetch('http://localhost:5678/api/works')
     .then(res => res.json())
-    .then(data => {
-        const gallery = document.querySelector(".gallery");
-        for (let i = 0; i < data.length; i++) {
-            const figure = document.createElement("figure");
-            const img = document.createElement("img");
-            const figcaption = document.createElement("figcaption");
-            img.src = data[i].imageUrl;
-            figcaption.innerHTML = data[i].title;
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-            gallery.appendChild(figure);
-        }
+    .then(response => {
+        works = response;
+        loadingWorks(works);
+        console.log(works)
     })
-    
+    .catch(function(error) {
+        console.error('Il y a eu un problème avec l\'opération fetch : ' + error.message);
+    });
+
+function loadingWorks(works) {
+    const gallery = document.querySelector(".gallery");
+    for (let i = 0; i < works.length; i++) {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        img.src = works[i].imageUrl;
+        figcaption.innerHTML = works[i].title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    }
+}
+
 // Etape 1.2 : Réalisation du filtre des travaux
+
+const filterAllWorks = document.querySelector(".filter-all-works");
+const filterObjects = document.querySelector(".filter-objects");
+const filterApartments = document.querySelector(".filter-apartments");
+const filterHotelsAndRestaurants = document.querySelector(".filter-hotels-and-restaurants");
+
+filterAllWorks.addEventListener("click", function () {
+    const categoryAllWorks = works.filter(function (allWorks) {
+        return (
+            allWorks.category.name === "Objets" ||
+            allWorks.category.name === "Appartements" ||
+            allWorks.category.name === "Hotels & restaurants"
+        );
+    });
+    document.querySelector(".gallery").innerHTML ="";
+    loadingWorks(categoryAllWorks)
+});
+
+filterObjects.addEventListener("click", function () {
+    const categoryObjects = works.filter(function (objects) {
+        return objects.category.name === "Objets";  
+    });
+    document.querySelector(".gallery").innerHTML ="";
+    loadingWorks(categoryObjects);
+});
+
+filterApartments.addEventListener("click", function () {
+    const categoryApartments = works.filter(function (apartments) {
+        return apartments.category.name === "Appartements";  
+    });
+    document.querySelector(".gallery").innerHTML ="";
+    loadingWorks(categoryApartments);
+})
+
+filterHotelsAndRestaurants.addEventListener("click", function () {
+    const categoryHotelsAndRestaurants = works.filter(function (hAndR) {
+        return hAndR.category.name === "Hotels & restaurants";  
+    });
+    document.querySelector(".gallery").innerHTML ="";
+    loadingWorks(categoryHotelsAndRestaurants);
+});
+
+// Etape 2 : Codez la page de connexion
+
+// Etape 2.1 : Intégration du design de la page de formulaire
+
+
+
+
 
 
 
